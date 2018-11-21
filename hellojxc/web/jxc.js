0dcf32179bb46b775844e3244f2dce3d862a1df0
marsday <marsday@gmail.com>
2018-11-21 21:45:39 +0800

    prepare to updatecustomer

--- hellojxc/web/jxc.js
+++ hellojxc/web/jxc.js
@@ -33,5 +33,117 @@ function show_addcustomer()
 }
 function clear_target()
 {
-    $("#target").html("");
+    $("#target").html('');
 }
+
+// list customers
+function list_addcustomer()
+{
+      $("#target").html('<form id="frm-example">'+
+            '<div class="container">' + 
+           '<a class="btn btn-primary" id="addcustomer" href="index.html?function=addcustomer" role="button">添加</a> ' + 
+           ' <button  id="del" role="button" class="btn btn-primary">删除</button>' + 
+           ' <button  id="update"role="button" class="btn btn-primary">更新</button>' + 
+        '</div>' + 
+    '<br>' + 
+       ' <table id="example" class="display select" width="100%" cellspacing="0">' + 
+       ' <thead>' + 
+        '   <tr>' + 
+        '     <th><input type="checkbox" name="select_all" value="1" id="example-select-all"></th>' + 
+        '     <th>name</th>' + 
+        '     <th>mobile</th>' + 
+        '     <th>location</th>' + 
+        '     <th>type</th>' + 
+        '   </tr>' + 
+        '</thead>' + 
+        '<tfoot>' + 
+        '   <tr>' + 
+        '     <th></th>' + 
+        '     <th>name</th>' + 
+        '     <th>mobile</th>' + 
+        '     <th>location</th>' + 
+        '     <th>type</th>' + 
+        '   </tr>' + 
+        '</tfoot>' + 
+         '</table>' + 
+    '</form>'
+    );
+    
+    var table = $('#example').DataTable({
+	   searching: false,
+      'ajax': {
+         'url': '/hellojxc/listcustomers'
+      },
+      'columnDefs': [{
+         'targets': 0,
+         'searchable': false,
+         'orderable': false,
+         'className': 'dt-body-left',
+         'render': function (data, type, full, meta){
+             return '<input type="checkbox" name="id[]" value="' + $('<div/>').text(data).html() + '">';
+         }
+      }],
+      'order': [[1, 'asc']]
+    });
+    
+    $("#del").on('click', function(){
+        $("#frm-example").attr("action","/hellojxc/delcustomer");
+        $("#frm-example").attr("method","POST");
+        $("#frm-example").submit();
+    });
+    $("#update").on('click', function(){
+      $("#frm-example").attr("action","/hellojxc/updatecustomer");
+      $("#frm-example").attr("method","POST");
+      $("#frm-example").submit(); 
+    });
+   // Handle click on "Select all" control
+   $('#example-select-all').on('click', function(){
+      // Get all rows with search applied
+      var rows = table.rows({ 'search': 'applied' }).nodes();
+      // Check/uncheck checkboxes for all rows in the table
+      $('input[type="checkbox"]', rows).prop('checked', this.checked);
+   });
+
+   // Handle click on checkbox to set state of "Select all" control
+   $('#example tbody').on('change', 'input[type="checkbox"]', function(){
+      // If checkbox is not checked
+      if(!this.checked){
+         var el = $('#example-select-all').get(0);
+         // If "Select all" control is checked and has 'indeterminate' property
+         if(el && el.checked && ('indeterminate' in el)){
+            // Set visual state of "Select all" control
+            // as 'indeterminate'
+            el.indeterminate = true;
+         }
+      }
+   });
+
+   // Handle form submission event
+   $('#frm-example').on('submit', function(e){
+      //alert("test");
+      var form = this;
+
+      // Iterate over all checkboxes in the table
+      table.$('input[type="checkbox"]').each(function(){
+         // If checkbox doesn't exist in DOM
+         if(!$.contains(document, this)){
+            // If checkbox is checked
+            if(this.checked){
+               // Create a hidden element
+               $(form).append(
+                  $('<input>')
+                     .attr('type', 'hidden')
+                     .attr('name', this.name)
+                     .val(this.value)
+               );
+               //alert("name: "+ this.name + " value:   "+ this.value);
+               alert("test");
+            }
+         }
+      });
+   });
+}
+
+   
+   
+   
